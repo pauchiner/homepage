ed56e380d30249f15c1b25a4703cec891f1073fb
Pau Garcia Chiner <pauchiner@192.168.1.133>
2023-12-12 10:59:10 +0100

    feat: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['prettier-plugin-astro'],
+  bracketSameLine: true,
+  bracketSpacing: false,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true
+};
