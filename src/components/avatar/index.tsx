938a1c05004978619747c8aa6af314e80e1fcf47
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2023-12-13 19:16:42 +0100

    create the avatar component

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Avatar = () => {
+  return (
+    <div id="avatar-container">
+      <img alt="pauchiner's avatar" src="/images/avatar.png" />
+    </div>
+  );
+};
+
+export default Avatar;
