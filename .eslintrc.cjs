354c6ac1f9a8e64cfee9af2f1d4cf862fa0027d7
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2023-12-14 03:40:04 +0100

    fix(eslint): remove the shareable configuration and update the `lint` script in the package.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,46 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:astro/recomended'],
   overrides: [
+    // Configuration for `.astro` file.
     {
       files: ['*.astro'],
+      plugins: ['astro'],
       parser: 'astro-eslint-parser',
+
       parserOptions: {
         parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro']
+        extraFileExtensions: ['.astro'],
+        sourceType: 'module'
+      },
+
+      env: {
+        'astro/astro': true,
+        es2020: true,
+        node: true
+      },
+
+      rules: {
+        'astro/no-unused-define-vars-in-style': 'error',
+        'astro/no-conflict-set-directives': 'error',
+        'astro/no-unused-css-selector': 'error',
+        'astro/no-set-html-directive': 'error'
+      }
+    },
+
+    // Configuration for the `<script>` tag.
+    {
+      files: ['**/*.astro/*.js', '*.astro/*.js'],
+
+      parserOptions: {
+        sourceType: 'module'
+      },
+
+      env: {
+        browser: true,
+        es2020: true
+      },
+
+      rules: {
+        'prettier/prettier': 'off'
       }
     }
   ]
