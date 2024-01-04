a0624efccba56717a8f6ce4618e83234be5de45f
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2024-01-04 18:50:12 +0100

    feat(env): add typescript support to the current enviroment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly CONTACT_FORM_ENDPOINT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
