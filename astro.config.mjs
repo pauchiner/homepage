8e062f8e12e8fe732da6f9c304063d9523d5aab9
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2024-01-04 19:33:40 +0100

    chore(vercel)!: add the vercel integration to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
+
+// Plugins
+import vercel from "@astrojs/vercel/serverless";
 import preact from '@astrojs/preact';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "server",
+  adapter: vercel()
 });
