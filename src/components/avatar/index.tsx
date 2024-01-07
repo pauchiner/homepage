254c547191dd9a376d6818955f0b07307fdeb305
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2024-01-07 21:54:02 +0100

    fix: change the format of the images from `.png` to `.webp`

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 const Avatar = () => {
   return (
     <div id="avatar-container">
-      <img alt="pauchiner's avatar" src="/images/avatar.png" />
+      <img alt="pauchiner's avatar" src="/images/avatar.webp" />
     </div>
   );
 };
