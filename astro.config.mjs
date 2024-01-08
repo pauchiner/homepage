db015890821984fb6c3ec2455c7f9d124384629e
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2024-01-08 11:51:44 +0100

    feat(vercel): add speed insight analysis

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
   integrations: [preact()],
   output: "server",
   adapter: vercel({
+    speedInsights: {
+      enabled: true
+    },
     webAnalytics: {
       enabled: true
     }
