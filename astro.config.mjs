25c603e5f99a9bd90d78f061ac04ed673f0d3864
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2024-01-09 12:10:27 +0100

    fix: change the server mode to static outputs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import {defineConfig} from 'astro/config';
 
 // Plugins
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true
