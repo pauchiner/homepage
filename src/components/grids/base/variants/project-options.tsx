2f1459ac0cf9ad3d8bf51e98b752eeda874a997a
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2024-01-10 13:35:35 +0100

    fix: update the base link import

--- src/components/grids/base/variants/project-options.tsx
+++ src/components/grids/base/variants/project-options.tsx
@@ -1,14 +1,12 @@
 import {TotalProjectsBadge} from 'components/badges';
 import {BackIcon} from 'components/icons';
-import Link from 'components/links';
+import {BackLink} from 'components/links';
 import BaseGrid from '..';
 
 export const ProjectOptions = () => (
   <BaseGrid>
     <TotalProjectsBadge>13</TotalProjectsBadge>
-    <Link id="back-button" href="/" icon={<BackIcon />} removeTopMargin>
-      Back
-    </Link>
+    <BackLink removeTopMargin> Back </BackLink>
   </BaseGrid>
 );
 
