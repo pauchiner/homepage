8116e8e0bab0ebbd719d3f04c17db9dee3086ffa
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-14 16:07:52 +0100

    feat: add the sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // Plugins
 import vercel from '@astrojs/vercel/static';
 import preact from '@astrojs/preact';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  site: 'https://pauchiner.es',
+  integrations: [preact(), sitemap()],
   output: 'static',
   adapter: vercel({
     webAnalytics: {
