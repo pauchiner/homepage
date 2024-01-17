c712dfb8d540da76d0c4f0ffd2a5133d5c053639
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-17 11:28:49 +0100

    fix(grids): remove unused export

--- src/components/grids/index.ts
+++ src/components/grids/index.ts
@@ -3,5 +3,5 @@ export {
   LanguagesSkills,
   OthersSkills
 } from './skills/variants';
-export {SocialNetworks, ProjectOptions} from './base/variants';
+export {SocialNetworks} from './base/variants';
 export {CardsGrid} from './cards';
