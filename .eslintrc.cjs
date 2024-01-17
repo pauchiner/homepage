eb115e2c81ebeed54f582ba0ccb8427ec48b84d7
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-17 11:22:05 +0100

    chore: update eslint config based on withastro/docs repo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,48 +1,73 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   overrides: [
-    // Configuration for `.astro` files.
     {
       files: ['*.astro'],
-      plugins: ['astro'],
       parser: 'astro-eslint-parser',
-
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro'],
-        sourceType: 'module'
       },
-
-      env: {
-        'astro/astro': true,
-        es2020: true,
-        node: true
-      },
-
       rules: {
         'astro/no-unused-define-vars-in-style': 'error',
         'astro/no-conflict-set-directives': 'error',
         'astro/no-unused-css-selector': 'error',
         'astro/no-set-html-directive': 'error'
-      }
+      },
     },
-    // Configuration for the `<script>` tag.
     {
-      files: ['**/*.astro/*.js', '*.astro/*.js'],
-
-      parserOptions: {
-        sourceType: 'module'
-      },
-
-      env: {
-        browser: true,
-        es2020: true
-      },
-
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
-        'prettier/prettier': 'off'
-      }
-    }
-  ]
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_'},
+        ],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
+    },
+    {
+      files: ['*.d.ts'],
+      rules: {
+        '@typescript-eslint/triple-slash-reference': 'off',
+      },
+    },
+    {
+      files: ['*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_'},
+        ],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
+    },
+    {
+      // Define the configuration for `<script>` tag.
+      // Script in `<script>` is assigned a virtual file name with the `.js` extension.
+      files: ['**/*.astro/*.js', '*.astro/*.js'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
 };
