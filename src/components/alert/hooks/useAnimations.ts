91456cd1aa69762ea20576dc7a5036c8afc37ca3
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-19 19:19:11 +0100

    feat(alert): smooth scroll to the alert when is visible

--- src/components/alert/hooks/useAnimations.ts
+++ src/components/alert/hooks/useAnimations.ts
@@ -4,6 +4,8 @@ import {useEffect} from 'preact/hooks';
 const useAnimations = (props: AlertProps) => {
   useEffect(() => {
     const alert = document.querySelector(`#${props.id}`) as HTMLElement;
+    if (props.visible) alert.scrollIntoView({behavior: 'smooth'});
+
     alert.style.visibility = props.visible ? 'visible' : 'hidden';
     alert.style.height = props.visible ? 'inherit' : '0px';
     alert.style.transform = props.visible ? 'scaleY(1)' : 'scaleY(0.95)';
