d04817ca2c27ec24dc69774e4a4670b28e2ebe97
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-24 11:33:14 +0100

    perf(cards): add lazy loading to the projects card images

--- src/components/cards/project/index.tsx
+++ src/components/cards/project/index.tsx
@@ -14,7 +14,7 @@ export const ProjectCard = (props: Props) => {
   return (
     <BaseCard class="project-card">
       <div class="image">
-        <img src={props.image} alt="project preview" />
+        <img src={props.image} alt="project preview" loading="lazy" />
       </div>
       <div class="content">
         <h4>
