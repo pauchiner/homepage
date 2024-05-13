a5866db5c23ec93f2aa41df724953b1f31d22b8b
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-13 13:12:51 +0200

    chore: remove vercel as deploy target

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 import {defineConfig} from 'astro/config';
 
-// Plugins
-import vercel from '@astrojs/vercel/static';
 import preact from '@astrojs/preact';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pauchiner.es',
   integrations: [preact(), sitemap()],
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
+  output: 'static'
 });
