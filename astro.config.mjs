da22343f5c4031a4079f96f5f3118c93e5a5d0bd
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-13 13:15:04 +0200

    chore: add cloudflare pages as deploy target and enable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
-import preact from '@astrojs/preact';
+import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pauchiner.es',
   integrations: [preact(), sitemap()],
-  output: 'static'
+  output: 'server',
+  adapter: cloudflare()
 });
