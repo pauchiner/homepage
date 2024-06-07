9374e3fcd609f3a9b1ee6f39cf649d7b751895cf
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-07 10:15:54 +0200

    fix(components): update footer component content

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,9 +7,7 @@ const Footer = () => {
     <footer class={styles.footer}>
       <Divider />
       <p class={styles.paragraph}>
-        Designed with <Link href="https://figma.com">Figma</Link>, Built with{' '}
-        <Link href="https://astro.build">Astro</Link>, Deployed with{' '}
-        <Link href="https://vercel.com">Vercel</Link>.
+        Built with <Link href="https://astro.build">Astro</Link>
       </p>
     </footer>
   );
